d7533bc2b1d8ed17ac7ab6365ba5ce2d68a296a5
Dan Cowdry <medtc411@gmail.com>
2021-11-24 14:12:04 -0800

    up to student overides

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,17 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Test your exercise solutions with print statements here.
+            Student Dan = new Student("Dan", 1, 69, 4.20);
+            Console.WriteLine(Dan.GetGradeLevel());
+
+            Dan.AddGrade(40, 1.2);
+            Console.WriteLine($"Dan's new gpa after adding a course is {Dan.Gpa}");
+
+            Console.WriteLine(Dan);
+
+            Student violet = new Student("Violet", 2, 28, 3.5);
+            bool areTheSame = Dan.Equals(violet);
+            Console.WriteLine(areTheSame);
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -31,17 +31,58 @@ namespace SchoolPractice
         public void AddGrade(int courseCredits, double grade)
         {
             // Update the appropriate properties: NumberOfCredits, Gpa
+            double totalQualityScore = Gpa * NumberOfCredits;
+            totalQualityScore += courseCredits * grade;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
+
         }
 
         //TODO: Complete the GetGradeLevel method here:
-        public string GetGradeLevel(int credits)
+        public string GetGradeLevel(/*int credits*/)
         {
             // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+            if (NumberOfCredits <= 29)
+            {
+                return "freshman";
+            }
+            else if (NumberOfCredits <= 59)
+            {
+                return "sophomore";
+            }
+            else if (NumberOfCredits <= 89)
+            {
+                return "junior";
+            }
+            else
+            {
+                return "senior";
+            }
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
+        public override string ToString()
+        {
+            /*return Name + " (Credits: " + NumberOfCredits + ", GPA: " + Gpa + ")";*/
+            return $"Name: {Name} \n" +
+                $"Credits: {NumberOfCredits} \n" +
+                $"Grade Level: {GetGradeLevel()}\n" +
+                $"GPA: {Gpa}";
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StudentId);
+        }
+
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
